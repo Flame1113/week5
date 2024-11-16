4bc5f2efa81068905ae2094d335a846bf1779c42
Flame1113 <155918120+Flame1113@users.noreply.github.com>
2024-11-16 09:37:56 -0800

    Update week5.cpp

--- week5.cpp
+++ week5.cpp
@@ -14,43 +14,46 @@ int main(){
     cout << "Student name:" << setw(24) << "Jordon Marchesano" << endl;
     cout << "Student number:" << setw(14) << "900536880" << endl;
     cout << endl << endl;
-    string getMonth(int);
+    airport months[12];
+    string monthNames[12] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
     int singleDepart, singleArrive, bigInDay, smallInDay;
     int totalLandings = 0, totaltakeOffs = 0;
-    airport months[12];
+
     for (int rep = 0; rep < 12; rep++){
-        cout << "Please enter the number of planes that landed in " << getMonth(rep) << ": ";
+        cout << "Please enter the number of planes that landed in " << monthNames[rep] << ": ";
         cin >> singleDepart;
         months[rep].planeDepart = singleDepart;
         totaltakeOffs += singleDepart;
 
-        cout << "Please enter the number of planes that departed in " << getMonth(rep) << ": ";
+        cout << "Please enter the number of planes that departed in " << monthNames[rep] << ": ";
         cin >> singleArrive;
         months[rep].planeArrive = singleArrive;
         totalLandings += singleArrive;
 
-        cout << "Please enter the greatest number of planes that landed on a single day in " << getMonth(rep) << ": ";
+        cout << "Please enter the greatest number of planes that landed on a single day in " << monthNames[rep] << ": ";
         cin >> bigInDay;
         months[rep].mostInDay = bigInDay;
 
-        cout << "Please enter the least  number of planes that landed on a single day in " << getMonth(rep) << ": ";
+        cout << "Please enter the least  number of planes that landed on a single day in " << monthNames[rep] << ": ";
         cin >> smallInDay;
         months[rep].leastInDay = smallInDay;
     }
+    
     string mostMonth = "January", leastMonth = "January";
     int mostLand = months[0].mostInDay, smallLand = months[0].leastInDay;
-    for (int rep = 0; rep < 12; rep++){
-        if (months[rep].mostInDay > mostLand){
-            mostLand = months[rep].mostInDay;
-            mostMonth = getMonth(rep);
+    for (int i = 0; i < 12; i++){
+        if (months[i].mostInDay > mostLand){
+            mostLand = months[i].mostInDay;
+            mostMonth = monthNames[i];
         }
-        if (months[rep].leastInDay < smallLand){
-            smallLand = months[rep].leastInDay;
-            leastMonth = getMonth(rep);
+        if (months[i].leastInDay < smallLand){
+            smallLand = months[i].leastInDay;
+            leastMonth = monthNames[i];
         }
     }
-    cout << "The average monthly landings for the year is " << setPrecision(2) << totalLandings/12.0 << endl;
-    cout << "The average monthly departures for the year is " << setPrecision(2) << totaltakeOffs/12.0 << endl;
+
+    cout << "The average monthly landings for the year is " << setprecision(2) << totalLandings/12.0 << endl;
+    cout << "The average monthly departures for the year is " << setprecision(2) << totaltakeOffs/12.0 << endl;
     cout << "The total landings for the year is " << totalLandings << endl;
     cout << "The total departures for the year is " << totaltakeOffs << endl;
     cout << "The greatest number of planes that landed in a single day is " << mostLand << " which occured in the month of " << mostMonth << endl;
@@ -58,32 +61,4 @@ int main(){
     cin.ignore();
     cin.get();
     return 0;
-}
-string getMonth(int month){
-    switch (month){
-        case 0:
-            return "January";
-        case 1:
-            return "February";
-        case 2:
-            return "March";
-        case 3:
-            return "April";
-        case 4:
-            return "May";
-        case 5:
-            return "June";
-        case 6:
-            return "July";
-        case 7:
-            return "August";
-        case 8:
-            return "September";
-        case 9:
-            return "October";
-        case 10:
-            return "November";
-        case 11:
-            return "December";
-    }
 }
